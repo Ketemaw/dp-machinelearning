69b7cb475654593c4d53a773e2c6c80c5fe32626
Ketemaw Alebachew <119129128+Ketemaw@users.noreply.github.com>
2026-07-09 10:02:15 +0300

    Change message to info and load CSV data
    Updated message display and added data loading.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import  pandas as pd
 
 st.title('🚀 Machine Learning App')
 
-st.write("Let's Build ML App!")
+st.info("Let's Build ML App!")
+df=pd.read_csv('https://github.com/dataprofessor/data/blob/master/penguins_cleaned.csv')
