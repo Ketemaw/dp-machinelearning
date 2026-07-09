f18d29a9f63a591c45f1ef10ebea00d5ca3c9e11
Ketemaw Alebachew <119129128+Ketemaw@users.noreply.github.com>
2026-07-09 10:03:49 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import  pandas as pd
 st.title('🚀 Machine Learning App')
 
 st.info("Let's Build ML App!")
-df=pd.read_csv('https://github.com/dataprofessor/data/blob/master/penguins_cleaned.csv')
+df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
 df
